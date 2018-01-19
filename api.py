852010d4b2af570ad137694ba5727127472262e8
sulf1ron <reg@aalyp.cc>
2018-01-19 20:29:20 +0800

    Delete api.py

--- api.py
+++
@@ -1,8 +0,0 @@
-from flask import *
-from u2 import *
-import json
-app = Flask(__name__)
-@app.route('/profile', methods=['POST', 'GET'])
-def profile():
-	if request.method == 'POST':
-		return json.dumps(u2_profile(request.form['uid']))
